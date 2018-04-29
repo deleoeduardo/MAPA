32ce6447212fa784f73bf603c486cdf88c34be8e
Eduardo De Leo <deleoeduardo@gmail.com>
2018-04-29 19:38:25 -0300

    PROYECTO NODE JS
    Se subio todo el repo de node js
    - Descomprimir archivo node_modules.zip
    tuve que comprimir porque no me permitia subir la version online

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+express.static.mime.default_type = "text/html";
+app.use(express.static(path.resolve(__dirname,'../public')));
+
+
+app.get('/map.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "map.html" );
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
